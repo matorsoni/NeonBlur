117286203468f1498463dab4bfe2c7973351a49e
Mateus Orsoni <matorsoni45@gmail.com>
2024-04-06 00:23:24 -0300

    Initial setup #2
    Add OpenGL basic setup

---
+++ deps/glad/README.md
@@ -0,0 +1,3 @@
+GLAD files generated for OpenGL 3.3 Core.
+
+[Permalink](https://gen.glad.sh/#generator=c&api=gl%3D3.3&profile=gl%3Dcore%2Cgles1%3Dcommon)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 
+#define GLFW_INCLUDE_NONE
+#include "GLFW/glfw3.h"
+#include "glad/gl.h"
+
+static void error_callback(int error, const char* description) {
+    std::cerr << "GLFW Error: " << description << std::endl;
+}
+
 int main()
 {
     std::cout << "Hello NeonBlur" << std::endl;
+
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        std::cerr << "GLFW init failed!" << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "NeonBlur v0.1", NULL, NULL);
+    if (!window) {
+        std::cerr << "GLFW windows creation failed!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    gladLoadGL(glfwGetProcAddress);
+    glClearColor(0.36, 0.0, 0.4, 1.0);
+
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
